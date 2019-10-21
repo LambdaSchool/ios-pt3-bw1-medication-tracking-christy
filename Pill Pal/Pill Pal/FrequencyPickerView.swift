2e6de160547608ed8d43aadd44521731eaa911b0
CrystalKnightCodes <CrystalKnightCodes@gmail.com>
2019-10-20 22:40:39 -0400

    Configured frequency picker view data source and delegate in Add/Edit View Controller

--- Pill Pal/Pill Pal/FrequencyPickerView.swift
+++ Pill Pal/Pill Pal/FrequencyPickerView.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class FrequencyPickerView: UIPickerView {
 
+    
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.

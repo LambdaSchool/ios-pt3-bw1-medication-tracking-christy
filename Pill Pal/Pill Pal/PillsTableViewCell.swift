2d9db9b1f8cb5148342b4977b854ac86bc27c5a3
CrystalKnightCodes <CrystalKnightCodes@gmail.com>
2019-10-22 22:43:03 -0400

    Initial coding configuration of Pills Table View Cell.

--- Pill Pal/Pill Pal/PillsTableViewCell.swift
+++ Pill Pal/Pill Pal/PillsTableViewCell.swift
@@ -10,9 +10,35 @@ import UIKit
 
 class PillsTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    // MARK: - Outlets
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var quantityLabel: UILabel!
+    @IBOutlet weak var dosageLabel: UILabel!
+    @IBOutlet weak var dosageTypeLabel: UILabel!
+    @IBOutlet weak var frequencyLabel: UILabel!
+    @IBOutlet weak var nextPillAtLabel: UILabel!
+    
+    // MARK: - Properties
+    var pill: Pill? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    // MARK: - View
+    func updateViews() {
+        guard let pill = pill else { return }
+        // Convert UInt values to Strings
+        let quantityString = String(pill.numberOfPills)
+        let dosageString = String(pill.dosage)
+        
+        // Assign pill values to labels
+        nameLabel.text = pill.name
+        quantityLabel.text = quantityString
+        dosageLabel.text = dosageString
+        dosageTypeLabel.text = pill.dosageType
+        frequencyLabel.text = pill.frequency.rawValue
+       // nextPillAtLabel.text =  // Need to configure timers before I can complete this
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {

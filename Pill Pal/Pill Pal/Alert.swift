9c3d96603d5e6fa337a961dcfa363590e4e6f206
CrystalKnightCodes <CrystalKnightCodes@gmail.com>
2019-10-26 16:08:07 -0400

    Added Alert file

---
+++ Pill Pal/Pill Pal/Alert.swift
@@ -0,0 +1,14 @@
+//
+//  Alert.swift
+//  Pill Pal
+//
+//  Created by Christy Hicks on 10/26/19.
+//  Copyright © 2019 Knight Night. All rights reserved.
+//
+
+import Foundation
+class Alerts {
+
+    
+
+}
